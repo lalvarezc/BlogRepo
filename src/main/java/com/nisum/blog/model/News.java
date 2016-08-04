fc7679f47cb817c86d4de5ce03c95cc2af378df3
Yermaín Araya Villacura <yers@Mac-Yermain.local>
2016-08-04 12:52:07 -0400

    added News and NewsTest

---
+++ src/main/java/com/nisum/blog/model/News.java
@@ -0,0 +1,88 @@
+package com.nisum.blog.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class News {
+	private int id;
+	private String title;
+	private String content;
+	private Date date;
+	private Author author;
+	private ArrayList<String> tags;
+	//private ArrayList<Comment> comments;
+	
+	
+	public News(int id, String title, String content, Author author) {
+		setId(id);
+		setTitle(title);
+		setContent(content);
+		setDate(date);
+		setAuthor(author);
+		setTags(new ArrayList<String>());
+		//setComments(new ArrayList<Comment>());
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = new Date();
+	}
+
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	
+	public ArrayList<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(ArrayList<String> tags) {
+		this.tags = tags;
+	}
+
+
+	/*public ArrayList<Comment> getComments() {
+		return comments;
+	}
+
+	public void setComments(ArrayList<Comment> comments) {
+		this.comments = comments;
+	}*/
+
+}
---
+++ src/test/java/com/nisum/blog/model/NewsTest.java
@@ -0,0 +1,16 @@
+package com.nisum.blog.model;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class NewsTest {
+	
+	@Test
+	public void checkInstance(){
+		News noticia;
+//		noticia = new News();
+		
+//		assertNotNull(noticia);
+	}	
+}

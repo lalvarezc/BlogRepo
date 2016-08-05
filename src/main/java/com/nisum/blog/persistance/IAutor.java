3120a030939011b65ef2f8653923890f51e29b1d
rodrigoNisum <rcaceres@nisum.com>
2016-08-05 10:04:32 -0400

    Adding CRUD methods

--- src/main/java/com/nisum/blog/persistance/IAutor.java
+++ src/main/java/com/nisum/blog/persistance/IAutor.java
@@ -3,6 +3,7 @@ package com.nisum.blog.persistance;
 public interface IAutor {
 
     public void create();
-    public void edit();
-    public void selectAll();
+    public void read();
+    public void update();
+    public void delete();
 }

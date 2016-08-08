9156b4284986bd12ed03fb9788f25844521c36f4
JesuZConte <jesuzconte@gmail.com>
2016-08-08 12:04:18 -0400

    Read testeado

---
+++ src/main/java/Borrar/Prueba.java
@@ -0,0 +1,13 @@
+package Borrar;
+
+import com.nisum.blog.model.Author;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		Author autor = new Author();
+		autor.setId(1);
+
+	}
+
+}
